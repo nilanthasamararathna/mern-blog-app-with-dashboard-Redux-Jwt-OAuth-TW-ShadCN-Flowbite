b4c7e272b4caba81bf5e5230584802ed9082dad6
E.Nilantha Samararathna <45939807+nilanthasamararathna@users.noreply.github.com>
2025-02-05 13:53:17 +0530

    add a middleware and a function to handle errors

--- api/index.js
+++ api/index.js
@@ -1,26 +1,26 @@
-import express from "express";
-import mongoose from "mongoose";
+import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js'
-import authRoutes from './routes/auth.route.js'
-
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO)
-.then(() => {
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log('MongoDB is Connected');
-})
-.catch((err) => {
-  console.log(err);
-});
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.use(express.json());
 
 app.listen(3001, () => {
-    console.log('Server is running on port 3001');
+  console.log('Server is running on port 3001');
 });
 
 // app.get('/test', ( req, res ) => {
@@ -29,3 +29,14 @@ app.listen(3001, () => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internel Server Error';
+
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
